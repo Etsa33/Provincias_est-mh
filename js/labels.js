bf2fe2907303cc4bc1a0f45b51d16e769c599603
etsa <etsa.bustos@gmail.com>
2024-10-15 16:31:36 -0500

    antes_ult_corchete

--- js/labels.js
+++ js/labels.js
@@ -59,4 +59,17 @@ function addLabel(layer, id) {
         }
       }
   }
+L.Control.Watermark=L.Control.extend({
+            onAdd:function(map){
+                var img = L.DomUtil.create('img');
+                img.src = 'logo1.png';
+                img.style.width = '200px';
+                return img;
+                },
+                onRemove:function(map){},
+                });
+                L.control.watermark = function(opts){
+                    return new L.Control.Watermark(opts);
+                    }
+                L.control.watermark({position:'bottomleft'}).addTo(map);
 }
