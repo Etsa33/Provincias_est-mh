4a096bbcee589e7570e7089f248d7f54b98de0a4
etsa <etsa.bustos@gmail.com>
2024-10-15 16:38:08 -0500

    Create logo1.js

---
+++ js/logo1.js
@@ -0,0 +1,14 @@
+var map = L.map("map").setView([-1.808,-83.32],6); 
+L.Control.Watermark=L.Control.extend({
+            onAdd:function(map){
+                var img = L.DomUtil.create('img');
+                img.src = 'logo1.png';
+                img.style.width = '200px';
+                return img;
+                },
+                onRemove:function(map){},
+                });
+                L.control.watermark = function(opts){
+                    return new L.Control.Watermark(opts);
+                    }
+                L.control.watermark({position:'bottomleft'}).addTo(map)
